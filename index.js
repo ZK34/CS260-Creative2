87089c43bb1a336c7c6e2568258c0b5a99e74dc9
ZK34 <70702743+ZK34@users.noreply.github.com>
2022-02-19 15:32:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+var map;
+$(document).ready(function() {
+  $('#getInfo').click(function() {
+    $.getJSON('https://ipapi.co/json', function(data) {
+        console.log(data);
+        $("#getInfo").hide();
+        $("#spacer-1").hide();
+        $("#spacer-2").hide();
+        $("#json").text(JSON.stringify(data, null, 2));
+        $("#json").show();
+        let latlng = new google.maps.LatLng(data.latitude, data.longitude)
+        map.panTo(latlng);
+        map.setZoom(6);
+        let marker = new google.maps.Marker({
+          position: latlng,
+          map: map
+        });
+
+        var infoHTML = '';
+        for (var key in data) {
+          infoHTML += '<p>' + key + ': ' + data[key] + '</p>';
+        }
+        $('#ipInfo').html(infoHTML);
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('getJSON request failed! ' + textStatus);
+        console.log("incoming " + jqXHR.responseText);
+      });
+  });
+});
+
+function map() {
+  $("#map").height(window.innerHeight);
+  var centerUS = {
+    lat: 39.41,
+    lng: -111.95
+  };
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: centerUS
+  });
+}
